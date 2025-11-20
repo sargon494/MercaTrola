f780eda9a354ab117e0856b62421b4efabbaef2f
FelipeCGibaja <150200634+sargon494@users.noreply.github.com>
2025-11-20 16:58:56 +0100

    Se comienzan a realizar algunas pruebas con las colas por cada caja.

--- src/caja/clientes/CajaPorCliente.java
+++ src/caja/clientes/CajaPorCliente.java
@@ -1,5 +1,22 @@
 package caja.clientes;
 
-public class CajaPorCliente {
+import java.util.Random;
 
+public class CajaPorCliente {
+    private int idCaja;
+
+    public CajaPorCliente(int caja) {
+        this.idCaja = caja;
+    }
+
+    public synchronized void atenderClientes(Clientes cliente){
+        try {
+            System.out.println("Cliente " + Thread.currentThread().getName() + " está siendo atendido en la caja: " + idCaja);
+            Thread.sleep(new Random().nextInt(500) + 100);
+            System.out.println("Cliente " + Thread.currentThread().getName() + " ha terminado en la caja: " + idCaja);
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/caja/clientes/Clientes.java
+++ src/caja/clientes/Clientes.java
@@ -1,5 +1,29 @@
 package caja.clientes;
 
-public class Clientes  extends Thread implements Runnable{
+public class Clientes  extends Thread{
+    private int idCliente;
+    private long tiempoEspera;
+    private CajaPorCliente numCaja;
 
+    public Clientes(int idCliente, CajaPorCliente caja){
+        this.idCliente = idCliente;
+        this.numCaja = caja;
+    }
+
+
+    @Override
+    public void run(){
+        long llegada = System.currentTimeMillis();
+
+        numCaja.atenderClientes(this);
+
+        long salida = System.currentTimeMillis();
+        this.tiempoEspera = salida - llegada;
+
+    }
+
+
+    public int getIdCliente(){
+        return idCliente;
+    }
 }
